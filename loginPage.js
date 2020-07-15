72c7dd80f0b4b182a97681f43ae1ace817c0320c
Revanth Raghu <revabob@gmail.com>
2020-07-15 20:53:42 +0530

    added title shortcut icon

--- loginPage.js
+++ loginPage.js
@@ -4,7 +4,7 @@ window.onload = () => {
     document.querySelectorAll(".input-container").forEach(function(e){
         e.querySelector('input').addEventListener('blur', function(event){
             if(event.target.value !== '') {
-                e.querySelector('label').setAttribute('style', 'translate: 0px -20px; background-color: white; color: #0059ff; padding-left: 5px; padding-right: 5px; font-size: 15px;')
+                e.querySelector('label').setAttribute('style', 'translate: 0px -30px; background-color: white; color: #0059ff; padding-left: 5px; padding-right: 5px; font-size: 15px;')
             }
             else {
                 if(e.querySelector('label') !== null) {
