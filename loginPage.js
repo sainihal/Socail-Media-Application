d50dffc6a03bb6a3acbc02c3e57fee19889a4190
Revanth Raghu <revabob@gmail.com>
2020-07-15 18:58:19 +0530

    added modals for sigin and ped reset

--- loginPage.js
+++ loginPage.js
@@ -1,3 +1,5 @@
+// -------------- Handles input animation ---------------------
+
 window.onload = () => {
     document.querySelectorAll(".input-container").forEach(function(e){
         e.querySelector('input').addEventListener('blur', function(event){
@@ -10,3 +12,44 @@ window.onload = () => {
         })
     })
 }
+
+// -------------- Handles input animation ---------------------
+
+// -------------- Toggles Modals ---------------------
+
+var modal = document.querySelector("#sign-up-modal");
+var modal2 = document.querySelector("#reset-pwd-modal");
+var trigger = document.querySelector(".trigger");
+var reset_pwd = document.querySelector("#reset-pwd")
+var closeButton = document.querySelectorAll(".close-button");
+
+function toggleModal(ele = undefined) {
+    if(ele.target !== undefined && ele.target.tagName === 'A') {
+        modal2.classList.toggle('show-modal')
+    }
+    else if(ele.target !== undefined && ele.target.tagName === 'BUTTON') {
+        modal.classList.toggle('show-modal')
+    }
+    else {
+        if(modal.classList.contains('show-modal')) {
+            modal.classList.toggle('show-modal')
+        }
+        if(modal2.classList.contains('show-modal')) {
+            modal2.classList.toggle('show-modal')
+        }
+    }
+}
+
+function windowOnClick(event) {
+    if (event.target === modal || event.target === modal2) {
+        toggleModal(event.target);
+    }
+}
+reset_pwd.addEventListener("click", toggleModal)
+trigger.addEventListener("click", toggleModal);
+closeButton.forEach(e => {
+    e.addEventListener("click", toggleModal);
+})
+window.addEventListener("click", windowOnClick);
+
+// -------------- Toggles Modals ---------------------
